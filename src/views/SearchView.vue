120ed42b0b6d0e504db177c2f16c1287d7fc3f61
AlisonSerra <alison.serra@hotmail.fr>
2023-01-05 15:24:26 +0100

    fix format

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -3,3 +3,14 @@
     <h1>This is an search page</h1>
   </div>
 </template>
+
+<script>
+import SearchAdress from '@/components/SearchAdress.vue'
+
+export default {
+  name: 'SearchView',
+  components: {
+    SearchAdress
+  }
+}
+</script>
